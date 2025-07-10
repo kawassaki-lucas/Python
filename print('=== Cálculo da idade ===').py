4e1a8347b101c44dc049a5a2cf8013259214c350
kawassaki-lucas <kirossauro@gmail.com>
2025-07-10 09:09:59 -0300

    Add files via upload

---
+++ print('=== Cálculo da idade ===').py
@@ -0,0 +1,5 @@
+print('=== Cálculo da idade ===')
+ano_atual = 2025
+ano_nascimento = int(input('Digite o ano de nascimento: '))
+idade = ano_atual - ano_nascimento
+print('A sua idade é 'sfr+(idade))
