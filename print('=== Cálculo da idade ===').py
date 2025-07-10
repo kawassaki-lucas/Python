fa531bf1aa1ffa4618d1a3acfedf1ca733e41d26
kawassaki-lucas <kirossauro@gmail.com>
2025-07-10 09:14:02 -0300

    Update print('=== Cálculo da idade ===').py

--- print('=== Cálculo da idade ===').py
+++ print('=== Cálculo da idade ===').py
@@ -2,4 +2,4 @@ print('=== Cálculo da idade ===')
 ano_atual = 2025
 ano_nascimento = int(input('Digite o ano de nascimento: '))
 idade = ano_atual - ano_nascimento
-print('A sua idade é 'sfr+(idade))
+print('A sua idade é '+str(idade))
