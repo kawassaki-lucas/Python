46ebbd99c1124c0b011d994b65925f1b64536774
kawassaki-lucas <kirossauro@gmail.com>
2025-08-07 10:01:28 -0300

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Digite a nota da prova: "))
+nota2 = float(input("Digite a nota do trabalho: "))
+media = (nota1 + nota2)/2
+print("Média ",media)
+
+if(media >=9):
+    print("PD")
+else:
+    print("ED")
